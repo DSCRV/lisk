217fbf90c4e632a298fbe8a31ef18b71c6bce2ce
Akshay <nerdy@peppe.rs>
2020-10-09 18:06:19 +0530

    mimic guile's errors

--- src/Error.hs
+++ src/Error.hs
@@ -21,7 +21,7 @@ instance Show LispError where
     show (Parse e)        = "Parser Error: " ++ show e
     show (BadForm s expr) = "Bad Form: " ++ s ++ ": " ++ show expr
     show (ArgCount n es) = "Invalid arity, expected " ++ show n ++ ", got values: " ++ unwordsList es
-    show (UnknownFunction fn) = "Unknown reference to function: " ++ fn
+    show (UnknownFunction fn) = "Cannot apply function: " ++ fn
     show (TypeMismatch msg got) = "Type mismatch, expected " ++ msg ++ ", got: " ++ show got
 
 type LispResult = Either LispError
