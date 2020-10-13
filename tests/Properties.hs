8b08ea946d1fc73d8363efb633b8063f750520bf
Akshay <nerdy@peppe.rs>
2020-10-13 23:09:42 +0530

    rework test suite

--- tests/Properties.hs
+++ tests/Properties.hs
@@ -1,15 +1,25 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Properties where
 
-import           Parser                               (Expr (..),
-                                                       parseLispValue,
-                                                       parseQuote)
+import           Parser          (Expr (..), parseLispValue, parseQuote)
 
-import           Test.Framework.Providers.QuickCheck2
-import           Test.Framework.TH
 import           Test.QuickCheck
-import           Text.ParserCombinators.Parsec
 
 -- some tests would go here hopefully
 
-tests = $testGroupGenerator
+-- a filler test to test the test suite :^)
+qsort :: (Ord a) => [a] -> [a]
+qsort [] = []
+qsort [x] = [x]
+qsort (x:xs) = qsort left ++ [x] ++ qsort right
+    where left = filter (<= x) xs
+          right = filter (> x) xs
+
+checkList :: (Ord a) => [a] -> Bool
+checkList = ordered . qsort
+    where ordered []       = True
+          ordered [x]      = True
+          ordered (x:y:xs) = x <= y && ordered (y:xs)
+
+return []
+tests = $quickCheckAll
